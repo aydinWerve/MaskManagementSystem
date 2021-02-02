fe41d98661f59c94e012d411cc3753e9714096fb
Merve Aydin <08aydinmeve@gmail.com>
2021-02-02 19:51:22 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        //çıplak class kalmasın !!
+        //PersonManager personManager = new PersonManager(); // bir iş sınıfı başka bir iş sınıfında new leniyorsa yeniliklere ve değişimlere yeterince açık değildir
+
+        private IApplicantService _applicantService;
+
+        public PttManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+
+        public void GiveMask(Person person)
+        {
+            if(_applicantService.CheckPerson(person)) //default : personManager.CheckPerson(person) == true
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi.");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske verilemedi.");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person1 = new Person();
+            person1.FirstName = "MERVE";
+            person1.LastName = "AYDIN";
+            person1.DateOfBirthYear = 1996;
+            person1.NationalityId = 1235;
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+    }
+}
